8bdfbab424818628afbea37f4fdefd4af7cf0fde
kineticcrusher <2rooney@telus.net>
2024-05-27 14:57:43 -0700

    Added basic N-body gravity simulator

---
+++ Assets/Scripts/Environment/NBodyInstance.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class NBodyInstance : MonoBehaviour {
+    public float mass;
+    public Vector3 initialVelocity;
+    public bool isAffected = true; public bool isAffector = true;
+    Vector3 velocity;
+
+    void Awake() {
+        velocity = initialVelocity;
+    }
+
+    // implements newton's universal gravitational formula (F = G * m1*m2 / r^2)
+    public void CalculateGravitationalForce(NBodyInstance[] otherNBodies, double gravitationalConstant) {
+        if (isAffected)
+            foreach (NBodyInstance affector in otherNBodies)
+                if (affector != this && affector.isAffector) {
+                    float distance = Vector3.Distance(transform.position, affector.transform.position);
+                    Vector3 direction = (affector.transform.position - transform.position).normalized;
+                    double force = gravitationalConstant * ((mass * affector.mass) / (distance * distance));
+
+                    velocity += direction * ((float)force / mass);
+                }
+    }
+
+    public void UpdatePosition() {
+        transform.position += velocity;
+    }
+}
---
+++ Assets/Scripts/Environment/NBodyManager.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class NBodyManager : MonoBehaviour {
+    public const double gravitationalConstant = 0.001f; //real world value is 0.00000000006674;
+    NBodyInstance[] nBodies;
+
+    void Awake() {
+        nBodies = FindObjectsOfType<NBodyInstance>();
+    }
+
+    /* updating velocities and THEN positions in that order helps to prevent instability caused by things like
+     * the three-body problem.
+     */
+    void FixedUpdate() {
+        for (int i = 0; i < nBodies.Length; ++i) {
+            nBodies[i].CalculateGravitationalForce(nBodies, gravitationalConstant);
+        }
+
+        for (int i = 0; i < nBodies.Length; ++i) {
+            nBodies[i].UpdatePosition();
+        }
+    }
+}
