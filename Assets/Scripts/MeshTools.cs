19220099d86745d3d80c0d992360bd7e42dbac34
kineticcrusher <2rooney@telus.net>
2024-05-28 19:47:30 -0700

    Fixed plane gen to work with specified normal vector

--- Assets/Scripts/MeshTools.cs
+++ Assets/Scripts/MeshTools.cs
@@ -14,12 +14,19 @@ public static class MeshTools {
         Vector3[] verts = new Vector3[(resolution + 1) * (resolution + 1)];
         Vector2[] uv = new Vector2[verts.Length];
 
+        Vector3 perpend1 = new(normal.y, normal.z, normal.x);
+        Vector3 perpend2 = Vector3.Cross(normal, perpend1);
+
         for (int i = 0, x = 0; x <= resolution; ++x) {
             for (int z = 0; z <= resolution; ++z) {
-                float xFractionOfUnit = (1f / resolution) * x;
-                float zFractionOfUnit = (1f / resolution) * z;
-                verts[i] = new(xFractionOfUnit, 0, zFractionOfUnit);
+                float xFractionOfUnit = 1f / resolution * x;
+                float zFractionOfUnit = 1f / resolution * z;
+                float xOffsetToCentered = xFractionOfUnit - 0.5f;
+                float zOffsetToCentered = zFractionOfUnit - 0.5f;
+
+                verts[i] = (normal / 2) + (perpend1 * xOffsetToCentered) + (perpend2 * zOffsetToCentered);
                 uv[i] = new(xFractionOfUnit, zFractionOfUnit);
+
                 ++i;
             }
         }
@@ -30,10 +37,10 @@ public static class MeshTools {
         for (int x = 0; x < resolution; ++x) {
             for (int z = 0; z < resolution; ++z) {
                 tris[trianglesOffset] = vertexOffset;
-                tris[trianglesOffset + 1] = vertexOffset + 1;
-                tris[trianglesOffset + 2] = vertexOffset + resolution + 1;
-                tris[trianglesOffset + 3] = vertexOffset + resolution + 1;
-                tris[trianglesOffset + 4] = vertexOffset + 1;
+                tris[trianglesOffset + 1] = vertexOffset + resolution + 1;
+                tris[trianglesOffset + 2] = vertexOffset + 1;
+                tris[trianglesOffset + 3] = vertexOffset + 1;
+                tris[trianglesOffset + 4] = vertexOffset + resolution + 1;
                 tris[trianglesOffset + 5] = vertexOffset + resolution + 2;
 
                 ++vertexOffset;
